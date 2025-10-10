3fce63f8e810100f172b254ffc77792d89c5b8ac
Mark Calla <markcalla@byui.edu>
2025-10-10 12:15:47 -0500

    Week04: Don't use @for and ngfor-this duplicate items

--- cms/src/app/documents/document-list/document-list.component.ts
+++ cms/src/app/documents/document-list/document-list.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { Document } from '../document.model';
+
 
 @Component({
   selector: 'cms-document-list',
@@ -7,5 +9,17 @@ import { Component } from '@angular/core';
   styleUrl: './document-list.component.css'
 })
 export class DocumentListComponent {
+  @Output() selectedDocumentEvent = new EventEmitter<Document>();
 
+  documents: Document[] = [
+    new Document('1', 'Angular Docs', 'Official Angular documentation', 'https://angular.io', []),
+    new Document('2', 'TypeScript Handbook', 'Complete TypeScript reference', 'https://typescriptlang.org', []),
+    new Document('3', 'Bootstrap Guide', 'Bootstrap v3 Documentation', 'https://getbootstrap.com', []),
+    new Document('4', 'RxJS Guide', 'Reactive Extensions for JavaScript', 'https://rxjs.dev', []),
+    new Document('5', 'Node.js Docs', 'Official Node.js documentation', 'https://nodejs.org/en/docs', [])
+  ];
+
+  onSelectedDocument(document: Document) {
+    this.selectedDocumentEvent.emit(document);
+  }
 }
